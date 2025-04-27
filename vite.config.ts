7128cfbe8fea7f3f32e394f97e01ecc5f9c42eb2
neelshha <neelshha@gmail.com>
2025-04-27 13:57:52 +0530

    compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
+
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression() // Enable gzip compression for production builds
+    viteCompression({
+      algorithm: 'gzip', // Gzip compression
+      ext: '.gz',
+      deleteOriginFile: false
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress', // Brotli compression
+      ext: '.br',
+      compressionOptions: { level: 11 },
+      deleteOriginFile: false
+    })
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
